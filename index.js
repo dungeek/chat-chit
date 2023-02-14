855cd0c6339eed057a0607432aca2d8748e3990e
dungeek <dungntr.dev@gmail.com>
2023-02-14 23:00:50 +0700

    basic concept

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+
+//Main block
+{
+  const app = express();
+  const server = require("http").createServer(app);
+
+  const createSocket = require("socket.io");
+  const serverSocket = createSocket(server);
+
+  serverSocket.on("connection", (clientSocket) =>
+    handleSocketConnection(clientSocket)
+  );
+
+  app.use(express.static("public"));
+  app.get("/", (_, res) => res.sendFile("index.html"));
+
+  server.listen(process.env.PORT || 4000);
+}
+
+const databaseUsers = require("./databaseUsers.json");
+const connectedUsers = [];
+
+//SHELL
+{
+  function handleSocketConnection(clientSocket) {
+    clientSocket.emit("connected", { id: clientSocket.id });
+
+    clientSocket.on("authenticating-user", (data) => {
+      handleSocketAuthenticate(clientSocket, data);
+    });
+
+    clientSocket.on("send-message", (data) =>
+      handleSocketSendMessage(clientSocket, data)
+    );
+
+    clientSocket.on("disconnected", (_) =>
+      handleSocketDisconnetion(clientSocket)
+    );
+  }
+}
+
+//FUNCTION LAYER 1: authenticating-user
+{
+  function handleSocketAuthenticate(socket, data = {}) {
+    const { username, password } = data;
+
+    // check if a user with the specific username and password existing in database
+    const validated = validateUsernameAndPassword(username, password);
+
+    if (validated) {
+      socket.username = username; // Assign Nick-name used by authenticated user in the chatting platform
+      socket.emit("authenticated-user", {});
+      log(`${username} joined ${socket.id}`);
+
+      //If anyone access with an existing username in the chatting, force the existing user to disconnect and inform it
+      //Validate in connectedUser
+      forceUserToDisconnect(username, "joined somewhere ");
+      connectedUsers.push(socket);
+      setTimeout(() => forceUserToDisconnect(username, "timeout"), 696969);
+    }
+  }
+
+  //Function layer 2
+  function validateUsernameAndPassword(username, password) {
+    const matched = databaseUsers.some((user) => {
+      return user.username === username && user.password === password;
+    });
+    return matched;
+  }
+
+  //Function layer 2
+  function forceUserToDisconnect(username, reason = "unknow") {
+    const index = connectedUsers.findIndex(
+      (element) => element.username === username
+    );
+    //If there is an user posssessing identical username with username inputing from client
+    if (index !== -1) {
+      const identicalUser = connectedUsers[index];
+      identicalUser.emit("force-disconnect", { reason });
+      connectedUsers.splice(index, 1);
+    }
+  }
+}
+
+//FUNCTION lAYER 1: send-message
+{
+  function handleSocketSendMessage(socket, data = {}) {
+    const { destination, message } = data;
+    const sender = socket.username;
+    const receiver = !!destination ? to : "*";
+    const receivers =
+      receiver === "*"
+        ? [...connectedUsers]
+        : [
+            socket,
+            connectedUsers.find((sk) => {
+              return (
+                sk.connectedUsers &&
+                sk.username === destination &&
+                sk.username != socket.username
+              );
+            }),
+          ];
+
+    receivers.forEach((sk) => {
+      sk?.emit("receive-message", {
+        sendAt: Date.now(),
+        sender: sender,
+        receiver: receiver,
+        message: message,
+      });
+    });
+    log(`${sender} to ${receiver}: ${message}`);
+  }
+
+  //Function layer 2
+  function log(message = "??") {
+    console.log(`${Date.now()} ${message}`);
+  }
+}
+
+//FUNCTION LAYER 1: disconnected
+{
+  function handleSocketDisconnetion(socket) {
+    log(`${socket.username} left ${socket.id}`);
+  }
+}
---
+++ public/script.js
@@ -0,0 +1,90 @@
+//Main block
+{
+    // Initializes a socket for the client and emits a `connection` event
+    const socket = io(); //this socket means clientSocket in server, it emits event and handle itself
+    socket.on('connected', (_) => onSocketConnection(socket));
+    socket.on('authenticated-user', (_) => onSocketAuthenticate(socket));
+    socket.on('force-disconnect', (data) => onSocketForceDisconnet(socket, data = {}));
+}
+
+//Socket Connection phase
+{   
+    function onSocketConnection (socket) {
+        //Accumulate data to initialize authenticating phase 
+        const username = document.getElementById('username');
+        const password = document.getElementById('password');
+        const join = document.getElementById('join-button');
+    
+        addEventEnterToClick(username, join);
+        addEventEnterToClick(password, join);
+    
+        join.onclick = () => {
+            socket.emit('authenticating-user', 
+            {
+                username : username.value,
+                password : password.value
+            });
+        };
+    }
+
+    //sub-function
+    function addEventEnterToClick(elementToEnter, elementToClick) {
+        elementToEnter && (elementToEnter.onkeydown = (e) => {
+            e.code === 'Enter' && elementToClick?.click();
+        });
+    }
+    
+}
+
+//After authenticating Users phase then authenticate receiver to send message and display messages
+{
+    function onSocketAuthenticate(socket) {
+        permitSocketToSendMessage(socket);
+        permitSocketToReceiveMessage(socket);
+    }
+    
+    function permitSocketToSendMessage (socket) {
+         const message = document.getElementById('message');
+         const receiver = document.getElementById('receiver');
+         const send_button = document.getElementById('send-button');
+         
+         addEventEnterToClick(message, send_button);
+         addEventEnterToClick(receiver, send_button);
+
+         send_button.onclick = () => {
+            if(message.value !== ''){
+                socket.emit('send-message', 
+                {
+                    message: message.value,
+                    receiver: receiver.value    
+                }
+                )
+            }
+         };
+         
+         //Display chat box
+         document.getElementById('sign-in').hidden = true;
+         document.getElementById('chat-box').hidden = false;
+    }
+
+    function permitSocketToReceiveMessage(socket) {
+        socket.on('receive-message', (data) => onSocketReceiveMessage(data));
+    }
+
+    function onSocketReceiveMessage(data) {
+        const {sendAt , sender , receiver, message} = data; 
+        const element = document.createElement('div');
+        element.innerText = `${new Date(sendAt).toJSON()} ${sender} to ${receiver} : ${message}`;
+        document.getElementById('inbox').appendChild(element);    
+    }
+}
+
+//When failing in authenticating phase
+{
+    function onSocketForceDisconnet(socket, data = {}) {
+        const {reason} = data;
+        alert(`Another ${socket.username} ${reason}, please connect again!!!`);
+        window.location.reload();
+    }
+}
+
